641433f6e7982dabb961c90b54c523e78783e09e
michabeeri <michael.beeri@gmail.com>
2018-01-23 13:30:23 +0200

    working in even more cards

--- src/card.jsx
+++ src/card.jsx
@@ -3,25 +3,31 @@ import _ from 'lodash';
 
 class Card extends React.Component {
     getForkDelta () {
-        const forkDelta = _.get(this.props, 'paths', []).length
-        return <div className={`forkDelta-${forkDelta}`}>{forkDelta}</div>
+        const fork = _.get(this.props, 'fork', 0)
+        const forkStr = `${fork > 0 ? '+' : ''}${fork}`
+        return <div className={`forkDelta-${fork}`}>{forkStr}</div>
     }
 
     getPathInfo (path) {
         return (<div className={`pathInfo ${_.get(path, 'flags', []).join(' ')}`}>{_.compact([
+            // conditions
+            path.scouting && <div className="measureWithIcon"><div className="measureValue">{path.scouting}</div><div className="icon"><i className="fa fa-binoculars"></i></div></div>,
+
             // challanges
             path.monster && <div className="icon"><i className="fa fa-gavel" style={{color: '#4682b4'}}></i></div>,
             path.obstacle && <div className="icon"><i className="fa fa-exclamation-triangle" style={{color: '#4682b4'}}></i></div>,
             path.riddle && <div className="icon"><i className="fa fa-puzzle-piece" style={{color: '#4682b4'}}></i></div>,
 
             // rewards
-            path.north === 1 && <div className="icon"><i className="fa fa-compass" style={{color: '#4682b4'}}></i></div>,
-            path.north === -1 && <div className="icon"><i className="fa fa-compass" style={{color: '#fd6a02'}}></i></div>,
             path.minerals && <div className="icon"><i className="fa fa-diamond" style={{color: '#4682b4'}}></i></div>,
             path.hero && <div className="icon"><i className="fa fa-user" style={{color: '#4682b4'}}></i></div>,
             path.ritual && <div className="icon"><i className="fa fa-spinner" style={{color: '#4682b4'}}></i></div>,
             path.item && <div className="icon"><i className="fa fa-gift" style={{color: '#4682b4'}}></i></div>,
-            path.quest && <div className="icon"><i className="fa fa-book" style={{color: '#4682b4'}}></i></div>
+            path.quest && <div className="icon"><i className="fa fa-book" style={{color: '#4682b4'}}></i></div>,
+
+            // modifiers
+            path.north === 1 && <div className="icon"><i className="fa fa-compass" style={{color: '#4682b4'}}></i></div>,
+            path.north === -1 && <div className="icon"><i className="fa fa-compass" style={{color: '#fd6a02'}}></i></div>
         ])}
         </div>);
     }
